38021ac6c12c31bbfe6591c068c68c373f911f77
sabina929 <sabina.abbasova2909@gmail.com>
2019-09-26 21:59:03 +0400

    some changes

--- src/pages/Contribute.js
+++ src/pages/Contribute.js
@@ -6,7 +6,7 @@ function Contribute() {
     <div className="container contribute">
            <h3>Project Files and Contribute to Project</h3>
            <p>
-               You can download full project from <a href="https://github.com/sabina929/React_Character_App" target="_blank" rel="noopener noreferrer">this</a> github page.
+               You can download full project from <a href="https://github.com/sabina929/character_app" target="_blank" rel="noopener noreferrer">this</a> github page.
                Also, you can contribute this project.
            </p>
     </div>
